f3d0abd6e17bceec07689495355f24e1b8e30e9d
txusif <txusif@gmail.com>
2025-04-08 17:10:21 +0530

    add custom hooks

---
+++ src/hooks/useViewPort.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useViewPort() {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setViewportWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return { viewportWidth };
+}
