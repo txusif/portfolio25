c546a2c90189139e691b53141ef1f902de55ffd2
txusif <txusif@gmail.com>
2025-04-12 13:23:28 +0530

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router";
 import AppContext from "./context/AppContext";
 import { router } from "./router";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <AppContext>
       <RouterProvider router={router} />
+      <Analytics />
     </AppContext>
   );
 }
